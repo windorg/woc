82307b16039ebdaf44c1029108d0583bb60d0a9b
Artyom Kazak <artyom@artyom.me>
2021-12-11 07:34:38 +0400

    Refactor CSS

---
+++ components/commentComponent.tsx
@@ -0,0 +1,107 @@
+import { Card, Comment } from '@prisma/client'
+import { commentSettings } from '../lib/model-settings'
+import { Dropdown } from 'react-bootstrap'
+import React from 'react'
+import Link from 'next/link'
+import { renderMarkdown } from '../lib/markdown'
+import ReactTimeAgo from 'react-time-ago'
+import { BiLink, BiPencil, BiDotsHorizontal, BiTrashAlt, BiLockOpen, BiLock, BiShareAlt } from 'react-icons/bi'
+import { AiOutlinePushpin } from 'react-icons/ai'
+import copy from 'copy-to-clipboard'
+import { callUpdateComment } from '../pages/api/comments/update'
+import styles from './commentComponent.module.scss'
+
+function MenuCopyLink(props: { card: Card, comment: Comment }) {
+  return <Dropdown.Item
+    onClick={() => { copy(`https://windofchange.me/ShowCard?cardId=${props.card.id}#comment-${props.comment.id}`) }}>
+    <BiShareAlt className="icon" /><span>Copy link</span>
+  </Dropdown.Item>
+}
+
+function MenuPin(props: { pinned, updateComment }) {
+  return props.pinned
+    ?
+    <Dropdown.Item onClick={() => props.updateComment({ pinned: false })}>
+      <AiOutlinePushpin className="icon" /><span>Unpin</span>
+    </Dropdown.Item>
+    :
+    <Dropdown.Item onClick={() => props.updateComment({ pinned: true })}>
+      <AiOutlinePushpin className="icon" /><span>Pin</span>
+    </Dropdown.Item>
+}
+
+function MenuMakePrivate(props: { private, updateComment }) {
+  return props.private
+    ?
+    <Dropdown.Item onClick={() => props.updateComment({ private: false })}>
+      <BiLockOpen className="icon" /><span>Make public</span>
+    </Dropdown.Item>
+    :
+    <Dropdown.Item onClick={() => props.updateComment({ private: true })}>
+      <BiLock className="icon" /><span>Make private</span>
+    </Dropdown.Item>
+}
+
+
+export function CommentComponent(props: {
+  card: Card
+  comment: Comment
+  afterCommentUpdated: (comment: Comment) => void
+}) {
+  const { card, comment } = props
+  const settings = commentSettings(comment)
+  const isPrivate = settings.visibility === 'private'
+  const classes = `
+    ${styles.comment}
+    ${isPrivate ? styles.commentPrivate : ""}
+    ${settings.pinned ? styles.commentPinned : ""}
+    `
+
+  const updateComment = async (data) => {
+    const diff = await callUpdateComment({ commentId: comment.id, ...data })
+    props.afterCommentUpdated({ ...comment, ...diff })
+  }
+
+  return (
+    <div key={comment.id} id={`comment-${comment.id}`} className={classes}>
+      <div className="d-flex justify-content-between" style={{ marginBottom: ".3em" }}>
+        <span className="text-muted small d-flex">
+          <Link href={`/ShowCard?cardId=${card.id}#comment-${comment.id}`}>
+            <a className="d-flex align-items-center">
+              <BiLink className="me-1" />
+              <ReactTimeAgo timeStyle="twitter-minute-now" date={comment.createdAt} />
+            </a>
+          </Link>
+          {isPrivate && <span className="ms-2"> 🔒</span>}
+        </span>
+        <div className="d-inline-flex small text-muted" style={{ marginTop: "3px" }}>
+          <span className="link-button link-button-dashed d-flex align-items-center">
+            <BiPencil className="me-1" /><span>Edit</span>
+          </span>
+          <Dropdown className={`${styles.moreButton} link-button ms-3 d-flex align-items-center`}>
+            <Dropdown.Toggle as="span" className="d-flex align-items-center">
+              <BiDotsHorizontal className="me-1" /><span>More</span>
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <MenuCopyLink card={card} comment={comment} />
+              <MenuMakePrivate private={isPrivate} updateComment={updateComment} />
+              <MenuPin pinned={settings.pinned} updateComment={updateComment} />
+              <Dropdown.Divider />
+              <Dropdown.Item className="text-danger">
+                <BiTrashAlt className="icon" /><span>Delete</span>
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          {/* TODO implement editing and deletion */}
+        </div>
+      </div>
+      <div className="rendered-content">
+        {renderMarkdown(comment.content)}
+      </div>
+      {/* TODO render replies */}
+    </div>
+  )
+}
+
+
--- pages/ShowCard.tsx
+++ pages/ShowCard.tsx
@@ -3,26 +3,20 @@ import Head from 'next/head'
 import type { Board, User, Card, Comment } from '@prisma/client'
 import { prisma } from '../lib/db'
 import { cardSettings, commentSettings } from '../lib/model-settings'
-import { Badge, Breadcrumb, Button, Dropdown, Form } from 'react-bootstrap'
+import { Badge, Breadcrumb, Button, Form } from 'react-bootstrap'
 import { BoardsCrumb, UserCrumb, BoardCrumb, CardCrumb } from '../components/breadcrumbs'
 import React, { createRef, RefObject, useState } from 'react'
-import Link from 'next/link'
 import { Tiptap, TiptapMethods } from '../components/tiptap'
 import _ from 'lodash'
 import * as R from 'ramda'
-import { renderMarkdown } from '../lib/markdown'
 import { SuperJSONResult } from 'superjson/dist/types'
 import { deserialize, serialize } from 'superjson'
 import { canEditCard } from 'lib/access'
 import { getSession } from 'next-auth/react'
 import { callCreateComment } from './api/comments/create'
 import update from 'immutability-helper'
-import ReactTimeAgo from 'react-time-ago'
-import { BiLink, BiPencil, BiDotsHorizontal, BiPin, BiTrashAlt, BiLockOpen, BiLock, BiShareAlt } from 'react-icons/bi'
-import { AiOutlinePushpin } from 'react-icons/ai'
 import { Formik } from 'formik'
-import copy from 'copy-to-clipboard'
-import { callUpdateComment } from './api/comments/update'
+import { CommentComponent } from '../components/commentComponent'
 
 type Card_ = Card & {
   owner: User
@@ -57,87 +51,6 @@ export const getServerSideProps: GetServerSideProps<SuperJSONResult> = async (co
   return {
     props: serialize(props)
   }
-}
-
-function CommentComponent(props: {
-  card: Card
-  comment: Comment
-  afterCommentUpdated: (comment: Comment) => void
-}) {
-  const { card, comment } = props
-  const settings = commentSettings(comment)
-  const isPrivate = settings.visibility === 'private'
-  const cardClasses =
-    "woc-comment" +
-    (isPrivate ? " woc-comment-private" : "") +
-    (settings.pinned ? " woc-comment-pinned" : "")
-
-  const updateComment = async data => {
-    const diff = await callUpdateComment({ commentId: comment.id, ...data })
-    props.afterCommentUpdated({ ...comment, ...diff })
-  }
-
-  return (
-    <div key={comment.id} id={`comment-${comment.id}`} className={cardClasses}>
-      <div className="d-flex justify-content-between" style={{ marginBottom: ".3em" }}>
-        <span className="text-muted small d-flex">
-          <Link href={`/ShowCard?cardId=${card.id}#comment-${comment.id}`}>
-            <a className="d-flex align-items-center">
-              <BiLink className="me-1" />
-              <ReactTimeAgo timeStyle="twitter-minute-now" date={comment.createdAt} />
-            </a>
-          </Link>
-          {isPrivate && <span className="ms-2"> 🔒</span>}
-        </span>
-        <div className="d-inline-flex small text-muted" style={{ marginTop: "3px" }}>
-          <span className="link-button link-button-dashed d-flex align-items-center">
-            <BiPencil className="me-1" /><span>Edit</span>
-          </span>
-          <Dropdown className="link-button ms-3 d-flex align-items-center woc-comment-more">
-            <Dropdown.Toggle as="span" className="d-flex align-items-center">
-              <BiDotsHorizontal className="me-1" /><span>More</span>
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item className="d-flex align-items-center"
-                onClick={() => { copy(`https://windofchange.me/ShowCard?cardId=${card.id}#comment-${comment.id}`) }}>
-                <BiShareAlt className="icon" /><span>Copy link</span>
-              </Dropdown.Item>
-              {isPrivate
-                ?
-                <Dropdown.Item className="d-flex align-items-center" onClick={() => updateComment({ private: false })}>
-                  <BiLockOpen className="icon" /><span>Make public</span>
-                </Dropdown.Item>
-                :
-                <Dropdown.Item className="d-flex align-items-center" onClick={() => updateComment({ private: true })}>
-                  <BiLock className="icon" /><span>Make private</span>
-                </Dropdown.Item>
-              }
-              {settings.pinned
-                ?
-                <Dropdown.Item className="d-flex align-items-center" onClick={() => updateComment({ pinned: false })}>
-                  <AiOutlinePushpin className="icon" /><span>Unpin</span>
-                </Dropdown.Item>
-                :
-                <Dropdown.Item className="d-flex align-items-center" onClick={() => updateComment({ pinned: true })}>
-                  <AiOutlinePushpin className="icon" /><span>Pin</span>
-                </Dropdown.Item>
-              }
-              <Dropdown.Divider />
-              <Dropdown.Item className="d-flex align-items-center text-danger">
-                <BiTrashAlt className="icon" /><span>Delete</span>
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-
-          {/* TODO implement editing and deletion */}
-        </div>
-      </div>
-      <div className="rendered-content">
-        {renderMarkdown(comment.content)}
-      </div>
-      {/* TODO render replies */}
-    </div >
-  )
 }
 
 // TODO don't allow posting with empty content
