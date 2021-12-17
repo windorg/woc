41801ad8e25a2faed5a9ceb497b31a502110806e
Artyom Kazak <artyom@artyom.me>
2021-12-17 18:04:46 +0400

    Use storage state in tests

--- e2e/board-private.spec.ts
+++ e2e/board-private.spec.ts
@@ -1,14 +1,10 @@
 import { test, expect } from '@playwright/test'
 
-test('private boards should not be visible to others', async ({ page }) => {
-  // TODO use state to log in as Alice
-  await page.goto('/Boards')
-  await page.click('text=Log in')
-  await page.fill('[placeholder="email@example.com"]', 'alice@woc.test')
-  await page.fill('[placeholder="Password"]', 'test')
-  await page.click('text=Sign in with credentials')
+test.use({ storageState: 'alice.storageState.json' })
 
-  // Create a new private board as Alice
+test('private boards should not be visible to others', async ({ page, browser }) => {
+  // Create a new private board
+  await page.goto('/Boards')
   await page.click('text=+ New')
   await page.fill('[placeholder="Board title"]', 'Alice\'s private board')
   await page.check('input[name="private"]')
@@ -18,6 +14,7 @@ test('private boards should not be visible to others', async ({ page }) => {
   await page.click('text=Alice\'s private board')
   await page.waitForURL(/.*\/ShowBoard.*/)
   const boardUrl = page.url()
+
   // Check that Alice can see the board
   await expect(page.locator('body')).toHaveText(/.*Alice's private board.*/)
 
@@ -26,28 +23,25 @@ test('private boards should not be visible to others', async ({ page }) => {
   await page.waitForURL(/.*\/ShowUser.*/)
   const aliceUrl = page.url()
 
-  await page.click('text=Log out')
-
   // Log in as Bob
-  await page.click('text=Log in')
-  await page.fill('[placeholder="email@example.com"]', 'bob@woc.test')
-  await page.fill('[placeholder="Password"]', 'test')
-  await page.click('text=Sign in with credentials')
+  {
+    const bobContext = await browser.newContext({ storageState: 'bob.storageState.json' })
+    const bobPage = await bobContext.newPage()
+    // Expect that the board is not accessible by the direct link
+    await bobPage.goto(boardUrl)
+    await expect(bobPage.locator('h1')).toHaveText(/404/)
+    await expect(bobPage.locator('body')).not.toHaveText(/.*Alice's private board.*/)
+    // Expect that the board is not visible in Alice's profile
+    await bobPage.goto(aliceUrl)
+    await expect(bobPage.locator('body')).not.toHaveText(/.*Alice's private board.*/)
+  }
 
-  // Expect that the board is not accessible by the direct link
-  await page.goto(boardUrl)
-  await expect(page.locator('h1')).toHaveText(/404/)
-  await expect(page.locator('body')).not.toHaveText(/.*Alice's private board.*/)
-
-  // Expect that the board is not visible in Alice's profile
-  await page.goto(aliceUrl)
-  await expect(page.locator('body')).not.toHaveText(/.*Alice's private board.*/)
-
-  // Log out and check that the board is not visible in the public boards list
-  await Promise.all([
-    page.waitForNavigation(),
-    page.click('text=Log out')
-  ])
-  await page.goto('/Boards')
-  await expect(page.locator('body')).not.toHaveText(/.*Alice's private board.*/)
+  // Log out
+  {
+    const anonContext = await browser.newContext({ storageState: { cookies: [], origins: [] } })
+    const anonPage = await anonContext.newPage()
+    // Check that the board is not visible in the public boards list
+    await anonPage.goto('/Boards')
+    await expect(anonPage.locator('body')).not.toHaveText(/.*Alice's private board.*/)
+  }
 })
