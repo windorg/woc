72ee64107058e06d4a946e2b6d59639a37c5412f
Artyom Kazak <artyom@artyom.me>
2021-12-22 21:13:38 +0400

    Factor out an assertion

--- e2e/reply-delete.spec.ts
+++ e2e/reply-delete.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test'
-import { createBoard, createCard } from './util'
+import { createBoard, createCard, createComment, createReply, expectReplyGone } from './util'
 
 test.use({ storageState: 'alice.storageState.json' })
 
@@ -8,35 +8,21 @@ test.use({ storageState: 'alice.storageState.json' })
 test('You can delete your own replies', async ({ page }) => {
   const boardName = await createBoard(page, { navigate: true })
   await createCard(page, { navigate: true })
-
-  // Post a comment
-  await page.click('.woc-comment-form .tiptap')
-  await page.keyboard.type('Some comment')
-  await page.click('button:has-text("Post")')
-
-  // Post a reply
-  await page.click('text=Reply')
-  await page.click('.woc-reply-form .tiptap')
-  await page.keyboard.type('Some reply')
-  await page.click('button:has-text("Post a reply")')
+  const commentContent = await createComment(page)
+  const replyContent = await createReply(page, commentContent)
 
   // The reply should be visible
-  await expect(page.locator('_react=ReplyComponent')).toContainText('Some reply')
+  await expect(page.locator('_react=ReplyComponent')).toContainText(replyContent)
 
   // The reply should still be visible after a reload
   await page.reload()
-  await page.waitForSelector(`text=${boardName}`)
-  await expect(page.locator('_react=ReplyComponent')).toContainText('Some reply')
+  await page.waitForSelector('_react=ReplyComponent')
+  await expect(page.locator('_react=ReplyComponent')).toContainText(replyContent)
 
   // Delete the reply
-  await page.click('.woc-comment-replies >> text=More')
+  await page.click('_react=ReplyComponent >> text=More')
   await page.click('a[role="button"]:has-text("Delete")')
 
   // The reply should not be visible
-  await expect(page.locator('#layout')).not.toContainText('Some reply')
-
-  // The reply should still not be visible after a reload and should not be anywhere on the page, including props
-  await page.reload()
-  await page.waitForSelector(`text=${boardName}`)
-  await expect(page.locator('body')).not.toContainText('Some reply')
+  await expectReplyGone(page, replyContent)
 })
--- e2e/util.ts
+++ e2e/util.ts
@@ -1,4 +1,5 @@
-import { Page } from '@playwright/test'
+import { expect, Page } from '@playwright/test'
+import { prisma } from '../lib/db'
 import randomWords from 'random-words'
 
 export async function createBoard(
@@ -30,6 +31,7 @@ export async function createCard(
   page: Page,
   options?: { navigate?: boolean }
 ): Promise<string> {
+  expect(page.url().includes('/ShowBoard'))
   const name = randomWords(3).join(' ')
   await page.fill('[placeholder="Card title"]', name)
   await page.press('[placeholder="Card title"]', 'Enter')
@@ -40,4 +42,43 @@ export async function createCard(
     ])
   }
   return name
+}
+
+// Create a comment. Assumes we are at the card page
+export async function createComment(
+  page: Page
+): Promise<string> {
+  expect(page.url().includes('/ShowCard'))
+  const content = randomWords(4).join(' ')
+  await page.click('.woc-comment-form .tiptap')
+  await page.keyboard.type(content)
+  await page.click('button:has-text("Post")')
+  return content
+}
+
+// Create a reply for a comment with given content. Assumes we are at the card page
+export async function createReply(
+  page: Page,
+  comment: string
+): Promise<string> {
+  expect(page.url().includes('/ShowCard'))
+  const content = randomWords(4).join(' ')
+  const commentHandle = await page.waitForSelector(`*_react=CommentComponent >> :has-text("${comment}")`);
+  (await commentHandle.$('text=Reply'))?.click()
+  await page.click('.woc-reply-form .tiptap')
+  await page.keyboard.type(content)
+  await page.click('button:has-text("Post a reply")')
+  return content
+}
+
+// Expect a reply with certain text to not be present on the page and in the database
+export async function expectReplyGone(page: Page, replyContent: string) {
+  expect(page.url().includes('/ShowCard'))
+  await expect(page.locator('#layout')).not.toContainText(replyContent)
+  const reply = await prisma.reply.findFirst({
+    where: {
+      content: { contains: replyContent }
+    }
+  })
+  expect(reply === null)
 }
