81cea9a3a67818046c6306bdccba76bd5f96151e
Artyom Kazak <artyom@artyom.me>
2021-12-22 21:56:05 +0400

    Test reply creation separately

--- e2e/reply-create.spec.ts
+++ e2e/reply-create.spec.ts
@@ -3,18 +3,17 @@ import { createBoard, createCard, createComment, createReply, expectReplyGone } 
 
 test.use({ storageState: 'alice.storageState.json' })
 
-// TODO test that others' replies to your comments can be deleted
-
-test('You can delete your own replies', async ({ page }) => {
+test('You can reply to your own comments', async ({ page }) => {
   const boardName = await createBoard(page, { navigate: true })
   await createCard(page, { navigate: true })
   const commentContent = await createComment(page)
   const replyContent = await createReply(page, commentContent)
 
-  // Delete the reply
-  await page.click('_react=ReplyComponent >> text=More')
-  await page.click('a[role="button"]:has-text("Delete")')
+  // The reply should be visible
+  await expect(page.locator('_react=ReplyComponent')).toContainText(replyContent)
 
-  // The reply should not be visible
-  await expectReplyGone(page, replyContent)
+  // The reply should still be visible after a reload
+  await page.reload()
+  await page.waitForSelector('_react=ReplyComponent')
+  await expect(page.locator('_react=ReplyComponent')).toContainText(replyContent)
 })
