767ec66fb423c2db7fc71cefdf7c9993e4673415
Artyom Kazak <artyom@artyom.me>
2022-02-03 13:03:43 +0200

    Better privacy feed tests

--- e2e/privacy/comments.spec.ts
+++ e2e/privacy/comments.spec.ts
@@ -29,12 +29,35 @@ test('Private comments should not be visible to others', async ({ page, browser 
   expectNoLeakage(responses, [privateComment])
 })
 
-test('Private comments should not appear in feeds', async ({ page, browser }) => {
+test('Private comments (or from private cards, boards) should not appear in feeds', async ({ page, browser }) => {
   await createBoard(page, { navigate: true })
-  await createCard(page, { navigate: true })
+  const publicBoardUrl = page.url()
 
+  await page.goto(publicBoardUrl)
+  await createCard(page, { navigate: true })
+  const publicCardUrl = page.url()
+
+  await page.goto(publicBoardUrl)
+  await createCard(page, { private: true, navigate: true })
+  const privateCardUrl = page.url()
+
+  await page.goto('/Boards')
+  await createBoard(page, { private: true, navigate: true })
+  await createCard(page, { navigate: true })
+  const publicCardInPrivateBoardUrl = page.url()
+
+  await page.goto(publicCardUrl)
   const publicComment = await createComment(page)
-  const privateComment = await createComment(page, { private: true })
+  // Private comment
+  const privateComment1 = await createComment(page, { private: true })
+  // Public comment on a private card
+  await page.goto(privateCardUrl)
+  const privateComment2 = await createComment(page)
+  // Public comment on a public card in a private board
+  await page.goto(publicCardInPrivateBoardUrl)
+  const privateComment3 = await createComment(page)
+
+  const privateComments = [privateComment1, privateComment2, privateComment3]
 
   // Get Alice's profile URL
   await page.click('text=@alice')
@@ -57,13 +80,17 @@ test('Private comments should not appear in feeds', async ({ page, browser }) =>
     // After following, the new user can see the public comment in the feed
     await xPage.goto('/ShowFeed')
     await xPage.locator('.woc-feed-item', { hasText: publicComment }).waitFor()
-    // But not the private comment
-    await expect(xPage.locator('body')).not.toContainText(privateComment)
+    // But not any of the private comments
+    for (const privateComment of privateComments) {
+      await expect(xPage.locator('body')).not.toContainText(privateComment)
+    }
 
-    // Just in case: anon users can't see the private comment in the feed (the feed shouldn't work for them at all)
+    // Just in case: anon users can't see the private comments in the feed (the feed shouldn't work for them at all)
     await anonPage.goto('/ShowFeed')
     await expect(anonPage.locator('body')).not.toContainText(publicComment)
-    await expect(anonPage.locator('body')).not.toContainText(privateComment)
+    for (const privateComment of privateComments) {
+      await expect(anonPage.locator('body')).not.toContainText(privateComment)
+    }
   })
-  expectNoLeakage(responses, [privateComment])
+  expectNoLeakage(responses, privateComments)
 })
