50d440b198cda2d12ed3f9539da0c948f2619ed3
Artyom Kazak <artyom@artyom.me>
2022-04-19 23:42:18 +0200

    'npm run dev' actually doesn't work for tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,11 +12,6 @@ expect.extend({
 const config: PlaywrightTestConfig = {
   globalSetup: './playwright-setup.ts',
   timeout: 120_000, // 2min per test
-  webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
-    reuseExistingServer: true,
-  },
   use: {
     baseURL: "http://localhost:3000",
     // trace: 'retain-on-failure',
