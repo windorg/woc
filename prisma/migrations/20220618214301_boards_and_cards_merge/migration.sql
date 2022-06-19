ccf21ec0e731fc9f9ecbc190f58a403784a937bf
Artyom Kazak <artyom@artyom.me>
2022-06-19 13:50:10 +0200

    Fix the migration

--- prisma/migrations/20220618214301_boards_and_cards_merge/migration.sql
+++ prisma/migrations/20220618214301_boards_and_cards_merge/migration.sql
@@ -11,16 +11,14 @@ CREATE TYPE "card_type" AS ENUM('card', 'board');
 -- Cards gain some new stuff
 ALTER TABLE "cards" ADD COLUMN "children_order" UUID[];
 ALTER TABLE "cards" RENAME COLUMN "board_id" TO "parent_id";
-ALTER TABLE "cards" ADD CONSTRAINT "cards_ref_parent_id" FOREIGN KEY ("parent_id") REFERENCES "cards"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+ALTER TABLE "cards" ALTER COLUMN "parent_id" DROP NOT NULL;
 ALTER TABLE "cards" ADD COLUMN "type" "card_type" NOT NULL DEFAULT 'card';
 ALTER TABLE "cards" ALTER COLUMN "type" DROP DEFAULT;
 CREATE INDEX "cards_type_index" ON "cards" ("type");
 
--- Cards without parents will be boards, so we need to allow nullable parent_id
-ALTER TABLE "cards" ALTER COLUMN "parent_id" DROP NOT NULL;
-
 -- Move all rows from 'boards' to 'cards'
 DO LANGUAGE plpgsql $$
+DECLARE board boards;
 BEGIN
   FOR board IN SELECT * FROM "boards" LOOP
     INSERT INTO "cards" 
@@ -29,6 +27,9 @@ BEGIN
       (board.id, 'board', board.title, NULL, board.created_at, board.settings, board.card_order, board.owner_id);
   END LOOP;
 END$$;
+
+-- Create a foreign key for parents
+ALTER TABLE "cards" ADD CONSTRAINT "cards_ref_parent_id" FOREIGN KEY ("parent_id") REFERENCES "cards"("id") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 -- Now boards are actually gone
 DROP TABLE "boards";
