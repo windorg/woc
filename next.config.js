6add4778c880c22d5285b6ce0a7252b9320c06fb
Artyom Kazak <artyom@artyom.me>
2022-06-25 01:54:04 +0200

    Typecheck on 'npm dev'

--- next.config.js
+++ next.config.js
@@ -1,13 +1,26 @@
 /** @type {import('next').NextConfig} */
 
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
 module.exports = {
   reactStrictMode: true,
+
   images: {
     // We don't use next/image because we want 'next export' to work, but we still
     // have to specify a loader or else 'next export' complains
     loader: 'imgix',
     path: '/',
   },
+
+  webpack: (config, options) => {
+    const { dev, isServer } = options
+    // Do not run type checking twice:
+    if (dev && isServer) {
+      config.plugins.push(new ForkTsCheckerWebpackPlugin())
+    }
+    return config
+  },
+
   // Old /ShowBoard and /ShowCard pages
   async redirects() {
     return [
