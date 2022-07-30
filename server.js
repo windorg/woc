fa6a2eec00063bc115e1fb3f2e7c094ef0e3f6ff
Artyom Kazak <artyom@artyom.me>
2022-07-30 03:27:28 +0200

    CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const { default: axios } = require('axios')
 const express = require('express')
+const cors = require('cors')
 const next = require('next')
 
 const port = parseInt(process.env.PORT, 10) || 3000
@@ -9,12 +10,24 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+// See docs at https://www.npmjs.com/package/cors#configuring-cors-w-dynamic-origin
+const corsWhitelist = ['tauri://localhost', process.env.NEXT_PUBLIC_APP_URL]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (corsWhitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 app.prepare().then(() => {
   const server = express()
 
   let initialized = false
 
-  server.all('*', (req, res) => {
+  server.all('*', cors(corsOptions), (req, res) => {
     if (initialized || req.url === '/api/health') {
       return handle(req, res)
     } else {
