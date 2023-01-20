925962440e1376306b12211c74377d93e1602a6c
Artyom Kazak <artyom@artyom.me>
2023-01-20 23:25:56 +0100

    simple objects & smth

--- lib/access.ts
+++ lib/access.ts
@@ -9,7 +9,7 @@ import { cardSettings, commentSettings, replySettings } from './model-settings'
 export async function canSeeCard<T extends { id: Card['id']; ownerId: Card['ownerId'] }>(
   userId: User['id'] | null,
   card: T
-) {
+): Promise<boolean> {
   if (card.ownerId === userId) return true
   const fullCard = await prisma.card.findUniqueOrThrow({
     where: { id: card.id },
@@ -23,7 +23,7 @@ export async function canSeeCard<T extends { id: Card['id']; ownerId: Card['owne
 export function canEditCard<T extends { id: Card['id']; ownerId: Card['ownerId'] }>(
   userId: User['id'] | null,
   card: T
-) {
+): boolean {
   if (!userId) return false // logged-out users cannot edit anything
   return card.ownerId === userId
 }
@@ -31,26 +31,28 @@ export function canEditCard<T extends { id: Card['id']; ownerId: Card['ownerId']
 export async function canSeeComment<T extends { id: Comment['id']; ownerId: Comment['ownerId'] }>(
   userId: User['id'] | null,
   comment: T
-) {
+): Promise<boolean> {
   if (comment.ownerId === userId) return true
   const fullComment = await prisma.comment.findUniqueOrThrow({
     where: { id: comment.id },
     include: { card: { select: { id: true, ownerId: true } } },
   })
-  return commentSettings(fullComment).visibility === 'public' && (await canSeeCard(userId, fullComment.card))
+  return (
+    commentSettings(fullComment).visibility === 'public' &&
+    (await canSeeCard(userId, fullComment.card))
+  )
 }
 export function canEditComment<T extends { id: Comment['id']; ownerId: Comment['ownerId'] }>(
   userId: User['id'] | null,
   comment: T
-) {
+): boolean {
   if (!userId) return false // logged-out users cannot edit anything
   return comment.ownerId === userId
   // If this logic changes, you should also change the logic in card.tsx
 }
-export async function canReplyToComment<T extends { id: Comment['id']; ownerId: Comment['ownerId'] }>(
-  userId: User['id'] | null,
-  comment: T
-) {
+export async function canReplyToComment<
+  T extends { id: Comment['id']; ownerId: Comment['ownerId'] }
+>(userId: User['id'] | null, comment: T): Promise<boolean> {
   if (!userId) return false // logged-out users cannot reply to anything
   // You can reply iff you can see the comment.
   return canSeeComment(userId, comment)
@@ -59,24 +61,27 @@ export async function canReplyToComment<T extends { id: Comment['id']; ownerId: 
 export async function canSeeReply<T extends { id: Reply['id']; authorId: Reply['authorId'] }>(
   userId: User['id'] | null,
   reply: T
-) {
+): Promise<boolean> {
   if (reply.authorId === userId) return true
   const fullReply = await prisma.reply.findUniqueOrThrow({
     where: { id: reply.id },
     include: { comment: { select: { id: true, ownerId: true } } },
   })
-  return replySettings(fullReply).visibility === 'public' && (await canSeeComment(userId, fullReply.comment))
+  return (
+    replySettings(fullReply).visibility === 'public' &&
+    (await canSeeComment(userId, fullReply.comment))
+  )
 }
 export function canEditReply<T extends { id: Reply['id']; authorId: Reply['authorId'] }>(
   userId: User['id'] | null,
   reply: T
-) {
+): boolean {
   if (!userId) return false // logged-out users cannot edit anything
   return reply.authorId === userId
 }
 export function canDeleteReply<
   T extends { id: Reply['id']; authorId: Reply['authorId']; comment: Pick<Comment, 'ownerId'> }
->(userId: User['id'] | null, reply: T) {
+>(userId: User['id'] | null, reply: T): boolean {
   if (!userId) return false // logged-out users cannot delete anything
   return (
     // The reply's author can always delete their own reply
