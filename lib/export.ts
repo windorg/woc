836b39edde3cc559d37a2bb8d093023f2162b990
Artyom Kazak <artyom@artyom.me>
2023-02-02 23:45:45 +0100

    Kill isNextExport

--- lib/export.ts
+++
@@ -1,7 +0,0 @@
-import { NextPageContext } from 'next'
-import _ from 'lodash'
-
-// Detect 'next export'. We can't call any server-side functions during 'next export'.
-export function isNextExport(ctx: NextPageContext): boolean {
-  return typeof window === 'undefined' && (ctx.req === undefined || _.isEmpty(ctx.req.headers))
-}
