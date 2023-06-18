5ae7de011ac73cba34229e1907916b0cae7e9cb4
Artyom Kazak <artyom@artyom.me>
2023-06-18 13:00:05 +0200

    Upgrade Tiptap

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,14 +1,35 @@
 import { Parser, HtmlRenderer } from 'commonmark'
 import React from 'react'
 import _ from 'lodash'
+import * as R from 'ramda'
+import cheerio from 'cheerio'
+
+function removeTrailingBlankLines(x: string): string {
+  return R.dropLastWhile(R.isEmpty, x.split('\n')).join('\n')
+}
 
 export function markdownToHtml(markdown: string): string {
   const reader = new Parser()
   const writer = new HtmlRenderer({ safe: true })
-  return writer.render(reader.parse(markdown))
+  let html = writer.render(reader.parse(markdown))
+
+  // Remove trailing newlines in <pre><code>. Otherwise Tiptap shows a blank line at the end.
+  {
+    const $ = cheerio.load(html)
+    $('pre code').each((i, el) => {
+      const $el = $(el)
+      const text = $el.text()
+      $el.text(removeTrailingBlankLines(text))
+    })
+    html = $.html()
+  }
+
+  return html
 }
 
-export function RenderedMarkdown(props: React.HTMLAttributes<HTMLDivElement> & { markdown: string }) {
+export function RenderedMarkdown(
+  props: React.HTMLAttributes<HTMLDivElement> & { markdown: string }
+) {
   return React.createElement('div', {
     ..._.omit(props, ['markdown']),
     dangerouslySetInnerHTML: { __html: markdownToHtml(props.markdown) },
