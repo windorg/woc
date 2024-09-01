2829995cb1ea0fddd6b71cddfbf925b6a7d0af56
Emily <artyom@artyom.me>
2024-09-01 13:20:57 +0200

    restore job queue

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -1,6 +1,6 @@
-// There is no way to get Next.js to run something at server startup (without using a custom server), so instead we call
-// /api/health on server start (see server.js). It a) does the actual healthcheck and b) makes sure to init the things
-// we want to init.
+// There is no way to get Next.js to run something at server startup (without using a custom server), so instead we call /api/health on server start (see server.js).
+//
+// It a) does the actual healthcheck, and b) makes sure to init the things we want to init.
 
 import { NextApiResponse } from 'next'
 import { startJobQueueProcessing } from '@lib/job-queue'
@@ -10,7 +10,7 @@ let initCalled = false
 export default async function handler(req, res: NextApiResponse) {
   if (!initCalled) {
     initCalled = true
-    // await startJobQueueProcessing()
+    await startJobQueueProcessing()
   }
   res.send('OK')
 }
